a75495eb5d94c49bbd73c0af2b6e0cfdd6e14141
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2019-04-28 14:08:31 -0600

    Ejercicio 2

--- V Lecture/Task5.py
+++ V Lecture/Task5.py
@@ -17,7 +17,7 @@ os.chdir("/Users/Andrey/Desktop/Data-Science-Course/V Lecture")
 
 pd.set_option('display.max_rows', 1000)
 
-datos = pd.read_csv('Titanic.csv',delimiter=',',decimal=",",index_col=0)
+datos = pd.read_csv('Titanic.csv',delimiter=',',decimal=".",index_col=0)
 print(datos)
 
 
@@ -146,6 +146,7 @@ y_pos = np.arange(len(barras))
 plt.bar(y_pos, alto, color=['red','blue', 'yellow'])
 plt.xticks(y_pos, barras)
 
+
 # 1.e
 
 datos.head()
@@ -163,6 +164,11 @@ print("Graficos funcion de densidad para hijos o padres")
 densidad = datos["Parch"].plot(kind='density')
 
 
+print("Graficos funcion de densidad para edades")
+
+densidad = datos["Age"].plot(kind='density')
+
+
 print("Histogramas para hermanos o conyugues")
 
 densidad = datos["SibSp"].plot(kind='hist')
@@ -170,6 +176,10 @@ densidad = datos["SibSp"].plot(kind='hist')
 print("Histogramas para hijos o padres")
 
 densidad = datos["Parch"].plot(kind='hist')
+
+print("Histogramas para edades")
+
+densidad = datos["Age"].plot(kind='hist')
 
 import scipy.stats
 
@@ -223,3 +233,213 @@ print(corr)
 f, ax = plt.subplots(figsize=(10, 8))
 sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool), cmap=sns.diverging_palette(220, 10, as_cmap=True),
             square=True, ax=ax)
+
+
+
+# Ehercicio 2
+
+
+# 2.a
+
+import os
+import pandas as pd
+import numpy as np
+
+os.chdir("/Users/Andrey/Desktop/Data-Science-Course/V Lecture")
+
+pd.set_option('display.max_rows', 1000)
+
+datos = pd.read_csv('SAheart.csv',delimiter=';',decimal=".",index_col=0)
+print(datos)
+
+# 2.c
+
+print("******************************\n")
+
+print("Describe")
+
+print(datos.dropna().describe())
+
+print("******************************\n")
+
+print("Means")
+
+print(datos.mean(numeric_only=True))
+
+print("******************************\n")
+
+print("STDs")
+
+print(datos.median(numeric_only=True))
+
+print("******************************\n")
+
+print("Maximo")
+
+print(datos.max(numeric_only=True))
+
+print("******************************\n")
+
+print("Percentiles")
+
+print(datos.quantile(np.array([0,.25,.50,.75,1])))
+
+print("******************************\n")
+
+print("Datos en variables categóricas\n")
+
+print("Historial Familiar:")
+
+print(datos['famhist'].value_counts())
+
+print("******************************\n")
+
+print("Coronary Heart Disease:")
+
+print(datos['chd'].value_counts())
+
+print("******************************\n")
+
+print("TABLA CRUZADA - Historial familiar por Coronary Heart Disease:")
+
+famhist_chd = pd.crosstab(index=datos["famhist"], columns=datos["chd"])
+print(famhist_chd)
+
+
+#2.d
+
+import matplotlib.pyplot as plt
+
+print("Family History")
+
+famhist = pd.crosstab(index=datos["famhist"], columns="count") 
+
+alto = [famhist['count'][0], famhist['count'][1]]
+barras = ('PRESENT', 'ABSENT')
+y_pos = np.arange(len(barras))
+plt.bar(y_pos, alto, color=['red','blue'])
+plt.xticks(y_pos, barras)
+
+print("******************************\n")
+
+print("Coronary Heart Disease")
+
+chd = pd.crosstab(index=datos["chd"], columns="count") 
+
+alto = [chd['count'][0], chd['count'][1]]
+barras = ('Si', 'No')
+y_pos = np.arange(len(barras))
+plt.bar(y_pos, alto, color=['red','blue', 'yellow'])
+plt.xticks(y_pos, barras)
+
+#2.e
+
+datos.head()
+boxplots = datos.boxplot(return_type='axes')
+
+#2.f
+
+print("Graficos funcion de densidad para tobacco")
+
+densidad = datos["tobacco"].plot(kind='density')
+
+print("Graficos funcion de densidad para low densiity lipoprotein ")
+
+densidad = datos["ldl"].plot(kind='density')
+
+print("Graficos funcion de densidad para adiposity")
+
+densidad = datos["adiposity"].plot(kind='density')
+
+print("Graficos funcion de densidad para type A")
+
+densidad = datos["typea"].plot(kind='density')
+
+print("Graficos funcion de densidad para obesidad")
+
+densidad = datos["obesity"].plot(kind='density')
+
+print("Graficos funcion de densidad para alcohol")
+
+densidad = datos["alcohol"].plot(kind='density')
+
+print("Graficos funcion de densidad para edad")
+
+densidad = datos["age"].plot(kind='density')
+
+#Hisotgramas
+
+print("Histogramas para tobacco")
+
+densidad = datos["tobacco"].plot(kind='hist')
+
+print("Histogramas para low densiity lipoprotein ")
+
+densidad = datos["ldl"].plot(kind='hist')
+
+print("Histogramas para adiposity")
+
+densidad = datos["adiposity"].plot(kind='hist')
+
+print("Histogramas para type A")
+
+densidad = datos["typea"].plot(kind='hist')
+
+print("Histogramas para obesidad")
+
+densidad = datos["obesity"].plot(kind='hist')
+
+print("Histogramas para alcohol")
+
+densidad = datos["alcohol"].plot(kind='hist')
+
+print("Histogramas para edad")
+
+densidad = datos["age"].plot(kind='hist')
+
+import scipy.stats
+
+"""
+Para pruebas de normalidad siempre se plantean así las hipótesis.
+
+Hipótesis:
+
+H0: La muestra proviene de una distribución normal.
+
+H1: La muestra no proviene de una distribución normal.
+
+Nivel de Significancia: El nivel de significancia que se trabajará es de 0.05. Alpha=0.05
+
+Criterio de Decisión
+
+Si P < Alpha Se rechaza H0
+
+Si p >= Alpha No se rechaza H0, es decir, los datos SÍ siguen la normal
+"""
+
+obesity_alcohol = scipy.stats.shapiro(datos.iloc[:,6:7])
+print(obesity_alcohol)
+
+p_value = obesity_alcohol[1]
+print(p_value)
+
+alpha = 0.05
+if p_value > alpha:
+    print('Sí sigue la curva Normal (No se rechaza H0)')
+else:
+    print('No sigue la curva Normal (Se rechaza H0)')
+    
+# 2.g
+    
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.pairplot(datos, hue='obesity', size=2.5)
+
+x = datos.iloc[:]["obesity"]
+
+plt.plot(x,'o', color='black')
+
+y = datos.iloc[:]["age"]
+
+plt.plot(y,'o', color='red')
