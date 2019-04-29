8d1c0af60cd117eae3813279d7f199a941e0f187
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2019-04-28 20:12:23 -0600

    Ejercicio 3

--- V Lecture/Task5.py
+++ V Lecture/Task5.py
@@ -443,3 +443,155 @@ plt.plot(x,'o', color='black')
 y = datos.iloc[:]["age"]
 
 plt.plot(y,'o', color='red')
+
+#2.h
+
+corr = datos.corr()
+print("Matriz de correlaciones \n")
+print(corr)
+
+f, ax = plt.subplots(figsize=(10, 8))
+sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool), cmap=sns.diverging_palette(220, 10, as_cmap=True),
+            square=True, ax=ax)
+
+
+#Ejercicio 3
+
+import prince
+
+import matplotlib.pyplot as plt
+from prince import PCA
+class ACP:
+    def __init__(self, datos, n_componentes = 5): 
+        self.__datos = datos
+        self.__modelo = PCA(n_components = n_componentes).fit(self.__datos)
+        self.__correlacion_var = self.__modelo.column_correlations(datos)
+        self.__coordenadas_ind = self.__modelo.row_coordinates(datos)
+        self.__contribucion_ind = self.__modelo.row_contributions(datos)
+        self.__cos2_ind = self.__modelo.row_cosine_similarities(datos)
+        self.__var_explicada = [x * 100 for x in self.__modelo.explained_inertia_]
+    @property
+    def datos(self):
+        return self.__datos
+    @datos.setter
+    def datos(self, datos):
+        self.__datos = datos
+    @property
+    def modelo(self):
+        return self.__modelo
+    @property
+    def correlacion_var(self):
+        return self.__correlacion_var
+    @property
+    def coordenadas_ind(self):
+        return self.__coordenadas_ind
+    @property
+    def contribucion_ind(self):
+        return self.__contribucion_ind
+    @property
+    def cos2_ind(self):
+        return self.__cos2_ind
+    @property
+    def var_explicada(self):
+        return self.__var_explicada
+        self.__var_explicada = var_explicada
+    def plot_plano_principal(self, ejes = [0, 1], ind_labels = True, titulo = 'Plano Principal'):
+        x = self.coordenadas_ind[ejes[0]].values
+        y = self.coordenadas_ind[ejes[1]].values
+        plt.style.use('seaborn-whitegrid')
+        plt.scatter(x, y, color = 'gray')
+        plt.title(titulo)
+        plt.axhline(y = 0, color = 'dimgrey', linestyle = '--')
+        plt.axvline(x = 0, color = 'dimgrey', linestyle = '--')
+        inercia_x = round(self.var_explicada[ejes[0]], 2)
+        inercia_y = round(self.var_explicada[ejes[1]], 2)
+        plt.xlabel('Componente ' + str(ejes[0]) + ' (' + str(inercia_x) + '%)')
+        plt.ylabel('Componente ' + str(ejes[1]) + ' (' + str(inercia_y) + '%)')
+        if ind_labels:
+            for i, txt in enumerate(self.coordenadas_ind.index):
+                plt.annotate(txt, (x[i], y[i]))
+    def plot_circulo(self, ejes = [0, 1], var_labels = True, titulo = 'Círculo de Correlación'):
+        cor = self.correlacion_var.iloc[:, ejes].values
+        plt.style.use('seaborn-whitegrid')
+        c = plt.Circle((0, 0), radius = 1, color = 'steelblue', fill = False)
+        plt.gca().add_patch(c)
+        plt.axis('scaled')
+        plt.title(titulo)
+        plt.axhline(y = 0, color = 'dimgrey', linestyle = '--')
+        plt.axvline(x = 0, color = 'dimgrey', linestyle = '--')
+        inercia_x = round(self.var_explicada[ejes[0]], 2)
+        inercia_y = round(self.var_explicada[ejes[1]], 2)
+        plt.xlabel('Componente ' + str(ejes[0]) + ' (' + str(inercia_x) + '%)')
+        plt.ylabel('Componente ' + str(ejes[1]) + ' (' + str(inercia_y) + '%)')
+        for i in range(cor.shape[0]):
+            plt.arrow(0, 0, cor[i, 0] * 0.95, cor[i, 1] * 0.95, color = 'steelblue', 
+                      alpha = 0.5, head_width = 0.05, head_length = 0.05)
+            if var_labels:
+                plt.text(cor[i, 0] * 1.05, cor[i, 1] * 1.05, self.correlacion_var.index[i], 
+                         color = 'steelblue', ha = 'center', va = 'center')
+    def plot_sobreposicion(self, ejes = [0, 1], ind_labels = True, 
+                      var_labels = True, titulo = 'Sobreposición Plano-Círculo'):
+        x = self.coordenadas_ind[ejes[0]].values
+        y = self.coordenadas_ind[ejes[1]].values
+        cor = self.correlacion_var.iloc[:, ejes]
+        scale = min((max(x) - min(x)/(max(cor[ejes[0]]) - min(cor[ejes[0]]))), 
+                    (max(y) - min(y)/(max(cor[ejes[1]]) - min(cor[ejes[1]])))) * 0.7
+        cor = self.correlacion_var.iloc[:, ejes].values
+        plt.style.use('seaborn-whitegrid')
+        plt.axhline(y = 0, color = 'dimgrey', linestyle = '--')
+        plt.axvline(x = 0, color = 'dimgrey', linestyle = '--')
+        inercia_x = round(self.var_explicada[ejes[0]], 2)
+        inercia_y = round(self.var_explicada[ejes[1]], 2)
+        plt.xlabel('Componente ' + str(ejes[0]) + ' (' + str(inercia_x) + '%)')
+        plt.ylabel('Componente ' + str(ejes[1]) + ' (' + str(inercia_y) + '%)')
+        plt.scatter(x, y, color = 'gray')
+        if ind_labels:
+            for i, txt in enumerate(self.coordenadas_ind.index):
+                plt.annotate(txt, (x[i], y[i]))
+        for i in range(cor.shape[0]):
+            plt.arrow(0, 0, cor[i, 0] * scale, cor[i, 1] * scale, color = 'steelblue', 
+                      alpha = 0.5, head_width = 0.05, head_length = 0.05)
+            if var_labels:
+                plt.text(cor[i, 0] * scale * 1.15, cor[i, 1] * scale * 1.15, 
+                         self.correlacion_var.index[i], 
+                         color = 'steelblue', ha = 'center', va = 'center')
+                
+                
+import os
+import pandas as pd
+import numpy as np
+
+os.chdir("/Users/Andrey/Desktop/Data-Science-Course/V Lecture")
+
+pd.set_option('display.max_rows', 1000)
+
+datos = pd.read_csv('ImportacionesMexico.csv',delimiter=';',decimal=",",index_col=0)
+print(datos)
+
+acp = ACP(datos,n_componentes=3)
+acp.plot_plano_principal()
+
+
+#3.2
+
+acp.plot_circulo()
+
+"""
+Hay relacion positiva entre Nicaragua y Costa Rica así como entre 
+los paises que son parte del triangulo del norte (Honduras, Guatemala, El Salvador)
+Con esto podemos interpretar que los paises vecinos tienden a tomar decisiones de importacion
+similares a sus vecinos durante mismos periodos de tiempo.
+Podemos ver que Panama se matiene un poco mas al margen y no existe relacion con el
+resto del istmo
+"""
+
+#3.3
+
+acp.plot_plano_principal(ejes = [0, 1])
+acp.plot_circulo()
+
+#3.b
+acp.plot_plano_principal(ejes = [0, 2])
+
+acp.plot_circulo()
+
